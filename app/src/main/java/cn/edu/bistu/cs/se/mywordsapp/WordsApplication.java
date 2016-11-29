a932b28e034fb2f5d5e25a8876b6fdb428d3a7ac
shenhaiyu <543209619@qq.com>
2016-11-29 22:07:34 +0800

    Initial commit

---
+++ app/src/main/java/cn/edu/bistu/cs/se/mywordsapp/WordsApplication.java
@@ -0,0 +1,21 @@
+package cn.edu.bistu.cs.se.mywordsapp;
+
+import android.app.Application;
+import android.content.Context;
+
+/**
+ * Created by hbs on 2015-10-24.
+ */
+public class WordsApplication extends Application{
+    private static Context context;
+    public static Context getContext(){
+        return WordsApplication.context;
+    }
+
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        WordsApplication.context=getApplicationContext();
+    }
+}
